73a0f48c55468ac6673f048e96d37d1ab7c1fe67
TiffanyGB <tiffany.gbellile@gmail.com>
2024-10-03 16:25:29 +0200

    ok

--- pages/Populaire.py
+++ pages/Populaire.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import utils.api_spotify as api_spotify
+import matplotlib.pyplot as plt
+from PIL import Image
+
 
 st.set_page_config(layout="wide")
 
@@ -10,16 +14,77 @@ df = pd.read_csv('Spotify_Song_Attributes.csv')
 
 st.title('Tendances')
 
-st.write("### Chanteurs populaires")
-st.write(df[['artistName', 'genre', 'msPlayed']].drop_duplicates())
+st.write("### Artistes populaires")
+total_plays_by_artist = df.groupby('artistName')['msPlayed'].sum().reset_index()
+top_3_artistes = total_plays_by_artist.nlargest(3, 'msPlayed')
+st.write(top_3_artistes)
 
+#récupération des images de chacun
+access_token = api_spotify.get_access_token(api_spotify.client_id, api_spotify.client_secret)
+imagetop1 = api_spotify.search_artist_image(top_3_artistes.iloc[0]['artistName'], access_token)
+imagetop2 = api_spotify.search_artist_image(top_3_artistes.iloc[1]['artistName'], access_token)
+imagetop3 = api_spotify.search_artist_image(top_3_artistes.iloc[2]['artistName'], access_token)
 
+#Mise en page
+# html_code = f"""
+# <div style="display: flex; justify-content: space-around;">
+#     <div>
+#         <img src="{imagetop1[1]}" style="border-radius: 50%; width: 250px; height: 250px;">
+#         <p style="text-align: center;">1. {imagetop1.iloc[1]}</p>
+#     </div>
+#     <div>
+#         <img src="{imagetop2[1]}" style="border-radius: 50%; width: 250px; height: 250px;">
+#         <p style="text-align: center;">2. <b>{top_artist.iloc[1]['trackName']}</b> de  {imagetop2[0]}</p>
+#     </div>
+#     <div>
+#         <img src="{imagetop3[1]}" style="border-radius: 50%; width: 250px; height: 250px;">
+#         <p style="text-align: center;">3. <b>{top_artist.iloc[2]['trackName']}</b> de {imagetop3[0]}</p>
+#     </div>
+# </div>
+# """
+
+# # Utiliser Streamlit components pour intégrer du HTML
+# st.components.v1.html(html_code, height=350)
+
+a = api_spotify.search_artist_image('Ninho', access_token)
+st.image(a[1], caption='Ceci est un exemple d\'image', use_column_width=True)
 
 st.write("### Genres populaires")
 
 
+
 st.write("### Top du moment")
-top_artist = df.nlargest(3, 'msPlayed')[['artistName', 'genre', 'msPlayed']]
-st.write(df.nlargest(3, 'msPlayed')[['artistName', 'genre', 'msPlayed']])
+
+#Récupération du top3 des chansons les plus écoutées
+top_artist = df.nlargest(5, 'msPlayed')[['artistName', 'genre', 'msPlayed', 'trackName']].drop_duplicates()
+
+#récupération des images de chacun
+access_token = api_spotify.get_access_token(api_spotify.client_id, api_spotify.client_secret)
+imagetop1 = api_spotify.search_artist_image(top_artist.iloc[0]['artistName'], access_token)
+imagetop2 = api_spotify.search_artist_image(top_artist.iloc[1]['artistName'], access_token)
+imagetop3 = api_spotify.search_artist_image(top_artist.iloc[2]['artistName'], access_token)
+
+#Mise en page
+html_code = f"""
+<div style="display: flex; justify-content: space-around;">
+    <div>
+        <img src="{imagetop1}" style="border-radius: 50%; width: 250px; height: 250px;">
+        <p style="text-align: center;">1. <b>{top_artist.iloc[0]['trackName']}</b> de {imagetop1[0]}</p>
+    </div>
+    <div>
+        <img src="{imagetop2}" style="border-radius: 50%; width: 250px; height: 250px;">
+        <p style="text-align: center;">2. <b>{top_artist.iloc[1]['trackName']}</b> de  {imagetop2[0]}</p>
+    </div>
+    <div>
+        <img src="{imagetop3}" style="border-radius: 50%; width: 250px; height: 250px;">
+        <p style="text-align: center;">3. <b>{top_artist.iloc[2]['trackName']}</b> de {imagetop3[0]}</p>
+    </div>
+</div>
+"""
+
+# Utiliser Streamlit components pour intégrer du HTML
+st.components.v1.html(html_code, height=350)
+
+
 
 
---
+++ utils/api_spotify.py
@@ -0,0 +1,93 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+# Charger le fichier .env
+load_dotenv()
+
+# Accéder aux variables d'environnement
+client_id = os.environ.get('ID_CLIENT')
+client_secret = os.environ.get('CLIENT_SECRET')
+
+
+
+print(client_id)
+print(client_secret)
+def get_access_token(client_id, client_secret):
+    auth_url = 'https://accounts.spotify.com/api/token'
+    auth_response = requests.post(auth_url, {
+        'grant_type': 'client_credentials',
+        'client_id': client_id,
+        'client_secret': client_secret,
+    })
+    auth_response_data = auth_response.json()
+    return auth_response_data['access_token']
+
+
+
+def search_track(track_name, artist_name, access_token):
+    search_url = 'https://api.spotify.com/v1/search'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    query_params = {
+        'q': f'track:{track_name} artist:{artist_name}',
+        'type': 'track',
+        'limit': 10  # Augmenter le nombre de résultats pour plus de choix
+    }
+    response = requests.get(search_url, headers=headers, params=query_params)
+    results = response.json()
+    tracks = results['tracks']['items']
+    if tracks:
+        for track in tracks:
+            # Vérifier que le nom de l'artiste et le titre correspondent exactement
+            if track_name.lower() in track['name'].lower() and any(artist_name.lower() in artist['name'].lower() for artist in track['artists']):
+                return track['id'], track['name'], track['artists'][0]['name'], track['preview_url']
+    return None, None, None, None
+
+def search_artist_image(artist_name, access_token):
+    search_url = 'https://api.spotify.com/v1/search'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    query_params = {
+        'q': f'artist:{artist_name}',
+        'type': 'artist',
+        'limit': 1  
+    }
+    response = requests.get(search_url, headers=headers, params=query_params)
+    results = response.json()
+    
+    if results['artists']['items']:
+        artist = results['artists']['items'][0]  
+        artist_name = artist['name']
+        artist_image_url = artist['images'][0]['url'] if artist['images'] else 'Aucune image trouvée'
+        return artist_name, artist_image_url
+    
+    return None, None
+
+
+
+def get_track_features(track_id, access_token):
+    track_features_url = f'https://api.spotify.com/v1/audio-features/{track_id}'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    response = requests.get(track_features_url, headers=headers)
+    return response.json()
+
+access_token = get_access_token(client_id, client_secret)
+
+track_id, found_track_name, found_artist_name, preview_url = search_track("La vie qu'on mène", "Ninh", access_token)
+a = search_artist_image('Nirvana', access_token)
+
+if track_id:
+    print(f"Found track ID: {track_id}")
+    print(f"Track name: {found_track_name}")
+    print(f"Artist name: {found_artist_name}")
+    print(f"Preview URL: {preview_url}")
+    track_features = get_track_features(track_id, access_token)
+    print(track_features)
+else:
+    print("No track found matching the criteria.")
+    
