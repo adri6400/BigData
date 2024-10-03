838654bec5a8bdf6c32ec78a17fd2c972017c1f1
TiffanyGB <tiffany.gbellile@gmail.com>
2024-10-03 10:02:05 +0200

    Page pop

--- pages/Populaire.py
+++ pages/Populaire.py
@@ -11,9 +11,15 @@ df = pd.read_csv('Spotify_Song_Attributes.csv')
 st.title('Tendances')
 
 st.write("### Chanteurs populaires")
-st.write(df[['artistName', 'genre']].drop_duplicates())
+st.write(df[['artistName', 'genre', 'msPlayed']].drop_duplicates())
 
 
 
 st.write("### Genres populaires")
+
+
+st.write("### Top du moment")
+top_artist = df.nlargest(3, 'msPlayed')[['artistName', 'genre', 'msPlayed']]
+st.write(df.nlargest(3, 'msPlayed')[['artistName', 'genre', 'msPlayed']])
+
 
